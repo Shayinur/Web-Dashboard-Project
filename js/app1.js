7eee85301e313abee12e02565ff88bea1893b69e
Shayinur <shayshohret@gmail.com>
2022-11-15 13:18:00 -0800

    final edits

--- js/app1.js
+++ js/app1.js
@@ -114,3 +114,15 @@ let mobileChart = new Chart(mobileCanvas, {
     data: mobileData,
     options: mobileOptions
 });
+
+send.addEventListener('click', () => {
+    // ensure user and message fields are filled out
+    if (user.value === "" && message.value === "") {
+      alert("Please fill out user and message fields before sending");
+    } else if (user.value === "" ) {
+      alert("Please fill out user field before sending");
+    } else if (message.value === "" ) {
+      alert("Please fill out message field before sending");
+    } else {
+      alert(`Message successfully sent to: ${user.value}`);
+} });
