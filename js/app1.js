e1d3f7d51af934a28853a6c983387832fb588319
Shayinur <shayshohret@gmail.com>
2022-11-15 13:15:03 -0800

    created data for mobile users

--- js/app1.js
+++ js/app1.js
@@ -3,6 +3,8 @@ const user = document.getElementById("userField");
 const message = document.getElementById("messageField");
 const send = document.getElementById("send");
 const trafficCanvas = document.getElementById("traffic-chart");
+const dailyCanvas = document.getElementById("daily-chart");
+const mobileCanvas = document.getElementById("mobile-chart");
 
 // create the html for the banner
 
@@ -47,7 +49,7 @@ let trafficOptions = {
     options: trafficOptions
   });
 
-  const dailyCanvas = document.getElementById("daily-chart");
+
 
   //daily traffic bar chart data
 
@@ -79,4 +81,36 @@ const dailyChart = new Chart (dailyCanvas, {
     type: 'bar',
     data: dailyData,
     options: dailyOptions
+});
+
+//mobile user data
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+      label: '# of Users',
+      data: [2000, 550, 500],
+      borderWidth: 0,
+      backgroundColor: [
+        '#7477BF',
+        '#78CF82',
+        '#51B6C8'
+  ] }]
+};
+
+const mobileOptions = {
+    aspectRatio: 1.9,
+    plugins: {
+      legend: {
+        position: 'right',
+        labels: {
+          boxWidth: 20,
+          fontStyle: 'bold'
+        }
+  } }
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
 });
